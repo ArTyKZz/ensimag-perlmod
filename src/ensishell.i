c74b0c6986d0c017ba6e6281a2445baf87d3b4e3
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-04-24 17:10:49 +0200

    source de base du module

---
+++ src/ensishell.i
@@ -0,0 +1,6 @@
+%module ensishell
+%{
+  extern int executer(char *);
+%}
+
+extern int executer(char *);
---
+++ src/executer.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static const int MAXARGS=1024; 
+
+/**
+   Fonction construisant un tableau de chaines de caracteres, termine
+   par NULL, à partir d'une seule chaine ou les mots sont separes par
+   des espaces.
+*/
+static char **splitspace(char *cmd) {
+  char *charcontext;
+  char **args = malloc(sizeof(char*) * MAXARGS);
+  memset(args, 0, sizeof(char*) * MAXARGS);
+  
+  char *res = strtok_r(cmd, " ", &charcontext);
+  args[0] = res;
+  int i=0;
+  while(res != NULL) {
+    res = strtok_r(NULL, " ", &charcontext);
+    ++i;
+    args[i] = res;
+  }
+  args[i] = NULL;
+  return args;
+};
+
+/**
+   Fonction appellee par l'interpréteur perl pour executer une
+   commande. Elle renvoie 0 si tout c'est bien passé et -1 sinon.
+
+   !!! FONCTION A IMPLANTER !!!
+*/
+int executer(char *cmd) {
+  printf("### Execution de la commande %s\n", cmd);
+
+  char **args = splitspace(cmd);
+  int i = 0;
+  while(args[i] != 0) {
+    printf("### Mot numéro %d: %s\n", i, args[i]);
+    i++;
+  }
+  
+  /* Mettre votre code ici (notamment) */ 
+
+  return 0;
+}
