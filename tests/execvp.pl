fa4f47ffba45da1be31598b4801e0b6c36e56623
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-04-26 12:06:20 +0200

    BUG: 2 execvp: 1 pour le perl et un pour le module

--- tests/execvp.pl
+++ tests/execvp.pl
@@ -3,7 +3,7 @@ use v5.10;
 open(my $fh, "strace -f perl ../tests/lsl.pl 2>&1 | grep execve | wc -l |");
 $ligne = <$fh>;
 chomp($ligne);
-if ($ligne > 0) {
+if ($ligne > 1) {
 	exit 0;
 } else {
 	exit -1;
